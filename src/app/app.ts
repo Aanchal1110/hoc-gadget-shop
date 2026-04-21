23ea973bc66e5f6845b8d3dd9e41ac193db18148
Aanchal <aanchalupadhyay1009@gmail.com>
2026-04-21 13:59:42 +0530

    Added inventory component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { Inventory } from './AppComponents/inventory/inventory';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, Inventory],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
