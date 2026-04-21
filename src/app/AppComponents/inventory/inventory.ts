f84bddb4c1ef309e28a413e3116e5718f743364d
Aanchal <aanchalupadhyay1009@gmail.com>
2026-04-21 18:30:29 +0530

    added ngmodel attribute and its value and names of the input field

--- src/app/AppComponents/inventory/inventory.ts
+++ src/app/AppComponents/inventory/inventory.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-inventory',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './inventory.html',
   styleUrl: './inventory.css',
 })
-export class Inventory {}
+export class Inventory {
+
+  inventory={
+    productID:"",
+    productName:"",
+    availableQty:0,
+    reorderPoint:0
+  }
+
+  onSubmit():void{
+    alert("Form submitted successfully"+JSON.stringify(this.inventory));
+  }
+}
