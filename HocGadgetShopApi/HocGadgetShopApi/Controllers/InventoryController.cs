78df92e1bb24f4728d394d5c7f719b6895d0dabc
Aanchal <aanchalupadhyay1009@gmail.com>
2026-04-25 12:06:44 +0530

    Added swagger earliear the application used openapi

--- HocGadgetShopApi/HocGadgetShopApi/Controllers/InventoryController.cs
+++ HocGadgetShopApi/HocGadgetShopApi/Controllers/InventoryController.cs
@@ -1,3 +1,4 @@
+using HocGadgetShopApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
@@ -10,12 +11,30 @@ namespace HocGadgetShopApi.Controllers
     public class InventoryController : ControllerBase
     {
         [HttpPost]
-        public ActionResult SaveInventory()
+        public ActionResult SaveInventory(InventoryRequestDto requestDto)
         {
-            SqlConnection connection=new SqlConnection
-            {
-                Connec
-            }
-        }
+      SqlConnection connection = new SqlConnection
+      {
+        ConnectionString = "Server=localhost;Database=gadgetshop;User Id=aanchal;Password=1105;TrustServerCertificate=True;"
+
+      };
+
+      SqlCommand command = new SqlCommand
+      {
+        CommandText = "sp_SaveInventory",
+        CommandType = System.Data.CommandType.StoredProcedure,
+        Connection = connection
+      };
+      command.Parameters.AddWithValue("@ProductId", requestDto.ProductId);
+      command.Parameters.AddWithValue("@ProductName", requestDto.ProductName);
+      command.Parameters.AddWithValue("@AvailableQty", requestDto.AvailableQty);
+      command.Parameters.AddWithValue("@ReOrderPoint", requestDto.ReOrderPoint);
+
+      connection.Open();
+      command.ExecuteNonQuery();
+      connection.Close();
+
+      return Ok("Inventory Details saved successfully");
+    }
     }
 }
