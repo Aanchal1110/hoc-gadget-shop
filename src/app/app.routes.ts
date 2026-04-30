6f087863dbe518c2c5a09a5b89345aef756b2740
Aanchal <aanchalupadhyay1009@gmail.com>
2026-04-30 21:13:38 +0530

    Routing done for customer component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Inventory } from './AppComponents/inventory/inventory';
+import { Customer } from './AppComponent/customer/customer';
 
-export const routes: Routes = [{path:'inventory',component:Inventory}];
+export const routes: Routes = [{path:'inventory',component:Inventory}, {path:'customer',component:Customer}];
 
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { Inventory } from './AppComponents/inventory/inventory';
+import { Customer } from './AppComponent/customer/customer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, Inventory],
+  imports: [RouterOutlet, RouterModule, Inventory, Customer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
