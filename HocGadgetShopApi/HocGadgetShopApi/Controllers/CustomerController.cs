4acc5403726eea9827d3fe3f7e5e376fba2cac4c
Aanchal <aanchalupadhyay1009@gmail.com>
2026-05-01 19:11:09 +0530

    Added the api to get the data in the backend

--- HocGadgetShopApi/HocGadgetShopApi/Controllers/CustomerController.cs
+++ HocGadgetShopApi/HocGadgetShopApi/Controllers/CustomerController.cs
@@ -2,6 +2,7 @@ using HocGadgetShopApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
 
 namespace HocGadgetShopApi.Controllers
 {
@@ -37,6 +38,49 @@ namespace HocGadgetShopApi.Controllers
       connection.Close();
 
       return Ok();
+    }
+
+
+    [HttpGet]
+    public ActionResult GetCustomer()
+    {
+      SqlConnection connection = new SqlConnection
+      {
+        ConnectionString = "Server=localhost;Database=gadgetshop;User Id=aanchal;Password=1105;TrustServerCertificate=True;"
+
+      };
+
+      SqlCommand command = new SqlCommand
+      {
+        CommandText = "sp_GetCustomerDetails",
+        CommandType = System.Data.CommandType.StoredProcedure,
+        Connection = connection
+      };
+     
+
+      connection.Open();
+      List<CustomerDto> customers = new List<CustomerDto>();
+
+      using(SqlDataReader reader = command.ExecuteReader())
+      {
+        while (reader.Read())
+        {
+          CustomerDto customerDto = new CustomerDto();
+          customerDto.CustomerId = Convert.ToInt32(reader["CustomerId"]);
+          customerDto.FirstName = Convert.ToString(reader["FirstName"])??"";
+          customerDto.LastName = Convert.ToString(reader["LastName"]) ?? "";
+          customerDto.Phone = Convert.ToString(reader["Phone"]) ?? "";
+          customerDto.Email = Convert.ToString(reader["Email"]) ?? "";
+          customerDto.RegistrationDate = Convert.ToDateTime(reader["RegistrationDate"]);
+
+          customers.Add(customerDto);
+
+        }
+      }
+      
+      connection.Close();
+
+      return Ok(JsonConvert.SerializeObject(customers));
     }
 
   }
---
+++ HocGadgetShopApi/HocGadgetShopApi/Models/CustomerDto.cs
@@ -0,0 +1,23 @@
+namespace HocGadgetShopApi.Models
+{
+  public class CustomerDto
+  {
+    
+  
+    #region Properties
+    public int CustomerId { get; set; }
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+    public string Email { get; set; }
+
+    public string Phone { get; set; }
+
+    public DateTime? RegistrationDate { get; set; }
+
+
+    #endregion
+  }
+}
+
+  
