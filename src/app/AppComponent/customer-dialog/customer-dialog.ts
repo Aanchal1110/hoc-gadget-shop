d4c1c95c5082833e194eae55d742042cfb545802
Aanchal <aanchalupadhyay1009@gmail.com>
2026-05-01 13:46:01 +0530

    Functionality to add customer using the dialog box to add

--- src/app/AppComponent/customer-dialog/customer-dialog.ts
+++ src/app/AppComponent/customer-dialog/customer-dialog.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { email } from '@angular/forms/signals';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-customer-dialog',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './customer-dialog.html',
   styleUrl: './customer-dialog.css',
 })
-export class CustomerDialog {}
+export class CustomerDialog {
+  httpClient=inject(HttpClient)
+
+  customer={
+    customerId:"",
+    firstName:"",
+    lastName:"",
+    registrationDate:"",
+    phone:"",
+    email:""
+  }
+onSubmit(){
+let apiUrl="http://localhost:5113/api/Customer";
+let httpOptions={
+
+headers:new HttpHeaders({
+  Authorization:'my-auth-token',
+  'Content-Type':'application/json'
+
+})
+}
+this.httpClient.post(apiUrl, this.customer, httpOptions).subscribe({
+  next:v=>console.log(v),
+  error:e=>console.log(e),
+  complete:()=>
+  {
+    alert("Customer details saved successfully: "+JSON.stringify(this.customer));
+}
+});
+}
+
+}
--- src/app/AppComponent/customer/customer.ts
+++ src/app/AppComponent/customer/customer.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CustomerDialog } from '../customer-dialog/customer-dialog';
 
 @Component({
   selector: 'app-customer',
@@ -6,4 +8,9 @@ import { Component } from '@angular/core';
   templateUrl: './customer.html',
   styleUrl: './customer.css',
 })
-export class Customer {}
+export class Customer {
+  private modalService=inject(NgbModal);
+  openCustomerDialog(){
+    this.modalService.open(CustomerDialog);
+  }
+}
